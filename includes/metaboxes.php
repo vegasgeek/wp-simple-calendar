6170a1eb5dca520c63f75a4d669b0b38c918fcf3
John Hawkins <john@vegasgeek.com>
2016-11-04 11:14:47 -0700

    cleaning up some text

--- includes/metaboxes.php
+++ includes/metaboxes.php
@@ -1,12 +1,7 @@
 <?php
 /**
- * Include and setup custom metaboxes and fields. (make sure you copy this file to outside the CMB2 directory)
- *
- * Be sure to replace all instances of 'yourprefix_' with your project's prefix.
- * http://nacin.com/2010/05/11/in-wordpress-prefix-everything/
- *
- * @category YourThemeOrPlugin
- * @package  Demo_CMB2
+ * @category WP Simple Calendar
+ * @package  WP Simple Calendar
  * @license  http://www.opensource.org/licenses/gpl-license.php GPL v2.0 (or later)
  * @link     https://github.com/WebDevStudios/CMB2
  */
@@ -35,14 +30,6 @@ function wpsimplecalendar_register_calendar_box() {
 		'id'            => 'wpsimplecalendar_event_meta',
 		'title'         => __( 'Event Details', 'cmb2' ),
 		'object_types'  => array( 'wpscevents', ), // Post type
-		// 'show_on_cb' => 'wpsimplecalendar_show_if_front_page', // function should return a bool value
-		// 'context'    => 'normal',
-		// 'priority'   => 'high',
-		// 'show_names' => true, // Show field names on the left
-		// 'cmb_styles' => false, // false to disable the CMB stylesheet
-		// 'closed'     => true, // true to keep the metabox closed by default
-		// 'classes'    => 'extra-class', // Extra cmb2-wrap classes
-		// 'classes_cb' => 'wpsimplecalendar_add_some_classes', // Add classes through a callback.
 	) );
 
 	$cmb->add_field( array(
@@ -82,7 +69,6 @@ function wpsimplecalendar_register_calendar_box() {
 		'desc'       => __( 'Full URL, including http://', 'cmb2' ),
 		'id'         => $prefix . 'url',
 		'type'       => 'text',
-		//'show_on_cb' => 'yourprefix_hide_if_no_cats', // function should return a bool value
 	) );
 
 	$cmb->add_field( array(
@@ -90,7 +76,6 @@ function wpsimplecalendar_register_calendar_box() {
 		'desc'       => __( 'For Example: "Click Here" or "For More Information"', 'cmb2' ),
 		'id'         => $prefix . 'reg_text',
 		'type'       => 'text',
-		//'show_on_cb' => 'yourprefix_hide_if_no_cats', // function should return a bool value
 	) );
 
 }
